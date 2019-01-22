17b21bb79fa76818bd61953e552cb3667c469c6c
zymla <32358213+zymla@users.noreply.github.com>
2019-01-22 17:25:17 +0100

    Optimizations Creation of shiny app for stations

---
+++ 10_clustering_stations_shiny/server.R
@@ -0,0 +1,109 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(lubridate)
+library(magrittr)
+library(data.table)
+library(leaflet)
+
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+    
+#= Load dataset ===========================================================
+    stations_filename <- list.files('../data_raw', pattern = 'divvy_stations_[0-9]{4}(_[0-9]{2}){2}_.*\\.csv', full.names = TRUE) %>% max()
+    print(stations_filename)
+    stations <- fread(stations_filename, na.strings = c(""))
+    print("Done loading data file")
+    
+#= Convert types of dataset ===========================================================
+    stations[
+        , 
+        `:=`(
+            available_bikes  = as.integer(available_bikes),
+            available_docks  = as.integer(available_docks),
+            docks_in_service = as.integer(docks_in_service),
+            latitude         = as.double(latitude),
+            longitude        = as.double(longitude),
+            percent_full     = as.integer(percent_full),
+            timestamp        = as_datetime(timestamp),
+            total_docks      = as.integer(total_docks)
+        ), 
+        ]
+#= Create features ===========================================================
+    stations[,tx_utilisation := available_bikes/docks_in_service]
+    stations[,date := as.Date(timestamp)]
+    tic()
+    # lubridate::wday is replaced by data.table::wday and a data.table join for higher performance 
+    #stations[, weekday := lubridate::wday(timestamp, label=TRUE, abbr = FALSE)]
+    wdays <- 
+        (
+            seq.Date(ymd('2019-01-21'), ymd('2019-01-28'), 'day') %>% as.data.table()
+        )[
+            , 
+            .(
+                wday_n   = data.table::wday(`.`),
+                wday_abr = lubridate::wday(`.`, label = TRUE)
+            ),
+            ][
+                , 
+                .N, 
+                .(wday_n, wday_abr)
+                ][
+                    , 
+                    .(wday_n, wday_abr)
+                    ]
+    stations[, `:=`(wday_n = data.table::wday(timestamp))]
+    stations <- stations[wdays, on = 'wday_n']
+    print(toc())
+    tic()
+    stations[, month := lubridate::month(timestamp, label=TRUE, abbr = FALSE)]
+    print("Done creating features")
+    
+#= Select data sample ===========================================================
+    stations3 <- 
+        stations[
+                weekday == "Tuesday" & id != 582
+            ][
+                , 
+                .(tx_utilisation = mean(tx_utilisation, na.rm = TRUE)), 
+                .(id, hr = hour(timestamp))][hr > 6 | hr < 3
+            ]
+    print("Done selecting sample")
+    
+#= Format data sample for kmeans ===========================================================
+    base3 <- stations3 %>% dcast(id ~ hr)
+    print("Done pivoting data")
+    
+    
+    output$latlongclusterPlot <- renderPlot({
+        #= kmeans clustering ===========================================================
+        #set.seed(1234)
+        km_cent <- input$nb_clusters # PARAMETRE A FAIRE VARIER
+        print(paste("Nb_cluster=", km_cent))
+        classifST <- kmeans(base3[,2:ncol(base3)], centers = km_cent) 
+        #  rapide, ajouter nstart = 100?
+        
+        stationsKM <- cbind(base3,classeKM = factor(classifST$cluster))
+        
+        # 5 - Ajout des coordonnées géographiques et visualisation
+        coor_st <- stations[!duplicated(id), .(id, latitude, longitude)]
+        stations_visu <- merge(stationsKM[, .(id, classeKM)], coor_st, by = "id")
+        factpal <- colorFactor(topo.colors(km_cent), stations_visu$classeKM)
+        stations_visu[id != 622] %>% 
+            ggplot() +
+            geom_point(data = stations_visu[id != 622, .(longitude, latitude)], aes(longitude, latitude), color = 'gray') +
+            geom_point(aes(longitude, latitude, color = classeKM)) +
+            scale_color_manual(values = factpal(1:km_cent)) +
+            facet_wrap(~classeKM)
+    })
+
+})
---
+++ 10_clustering_stations_shiny/ui.R
@@ -0,0 +1,33 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Divvy stations clustering"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("nb_clusters",
+                        "Number of clusters:",
+                        min = 1,
+                        max = 50,
+                        value = 5)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotOutput("latlongclusterPlot")
+        )
+    )
+))
